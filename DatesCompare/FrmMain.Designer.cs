9b89a1f102bd30fc2eba0bfaa8f306d5c16308e5
Cezar Lisboa <cezarsystems@gmail.com>
2021-10-15 12:27:14 -0300

    Add project files.

---
+++ DatesCompare/FrmMain.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DatesCompare
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtObservations = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpInicio = new System.Windows.Forms.DateTimePicker();
+            this.dtpFim = new System.Windows.Forms.DateTimePicker();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDataInicio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDataFim = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colObservacoes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDados.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colDataInicio,
+            this.colDataFim,
+            this.colObservacoes});
+            this.dgvDados.Location = new System.Drawing.Point(12, 148);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.ReadOnly = true;
+            this.dgvDados.RowHeadersVisible = false;
+            this.dgvDados.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
+            this.dgvDados.Size = new System.Drawing.Size(648, 290);
+            this.dgvDados.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(12, 51);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(206, 82);
+            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.Text = "SALVAR";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Data Início";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data Fim";
+            // 
+            // txtObservations
+            // 
+            this.txtObservations.Location = new System.Drawing.Point(224, 25);
+            this.txtObservations.Multiline = true;
+            this.txtObservations.Name = "txtObservations";
+            this.txtObservations.Size = new System.Drawing.Size(436, 108);
+            this.txtObservations.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Observações";
+            // 
+            // dtpInicio
+            // 
+            this.dtpInicio.CustomFormat = "dd/MM/yyyy";
+            this.dtpInicio.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpInicio.Location = new System.Drawing.Point(12, 25);
+            this.dtpInicio.Name = "dtpInicio";
+            this.dtpInicio.Size = new System.Drawing.Size(100, 20);
+            this.dtpInicio.TabIndex = 4;
+            this.dtpInicio.ValueChanged += new System.EventHandler(this.dtpInicio_ValueChanged);
+            // 
+            // dtpFim
+            // 
+            this.dtpFim.CustomFormat = "dd/MM/yyyy";
+            this.dtpFim.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFim.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dtpFim.Location = new System.Drawing.Point(118, 25);
+            this.dtpFim.Name = "dtpFim";
+            this.dtpFim.Size = new System.Drawing.Size(100, 20);
+            this.dtpFim.TabIndex = 4;
+            // 
+            // colID
+            // 
+            this.colID.FillWeight = 54.86425F;
+            this.colID.HeaderText = "Identificador";
+            this.colID.Name = "colID";
+            this.colID.ReadOnly = true;
+            // 
+            // colDataInicio
+            // 
+            this.colDataInicio.FillWeight = 72.47893F;
+            this.colDataInicio.HeaderText = "Data início";
+            this.colDataInicio.Name = "colDataInicio";
+            this.colDataInicio.ReadOnly = true;
+            // 
+            // colDataFim
+            // 
+            this.colDataFim.FillWeight = 50.76142F;
+            this.colDataFim.HeaderText = "Data Fim";
+            this.colDataFim.Name = "colDataFim";
+            this.colDataFim.ReadOnly = true;
+            // 
+            // colObservacoes
+            // 
+            this.colObservacoes.FillWeight = 221.8954F;
+            this.colObservacoes.HeaderText = "Observações";
+            this.colObservacoes.Name = "colObservacoes";
+            this.colObservacoes.ReadOnly = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(672, 450);
+            this.Controls.Add(this.dtpFim);
+            this.Controls.Add(this.dtpInicio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtObservations);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dgvDados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comparação de Datas";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtObservations;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpInicio;
+        private System.Windows.Forms.DateTimePicker dtpFim;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDataInicio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDataFim;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colObservacoes;
+    }
+}
+
---
+++ DatesCompare/FrmMain.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DatesCompare
+{
+    public class ListaItens
+    {
+        public int ID { get; set; }
+        public string DataIncio { get; set; }
+        public string DataFim { get; set; }
+        public string Observacoes { get; set; }
+    }
+
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            ResetDatas();
+        }
+
+        private void dtpInicio_ValueChanged(object sender, EventArgs e)
+        {
+            this.dtpFim.MinDate = this.dtpInicio.Value.AddDays(7);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtObservations.Text))
+            {
+                if (dgvDados.Rows.Count == 0)
+                {
+                    dgvDados.Rows.Add(dgvDados.Rows.Count + 1, dtpInicio.Value.ToString("dd/MM/yyyy"), dtpFim.Value.ToString("dd/MM/yyyy"), txtObservations.Text);
+
+                    ResetDatas();
+
+                    //MessageBox.Show("Sucesso ao incluir o agendamento", "Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);                   
+                }                    
+                else
+                {
+                    List<ListaItens> listaItens = new List<ListaItens>();
+
+                    foreach (DataGridViewRow row in dgvDados.Rows)
+                    {
+                        listaItens.Add(new ListaItens
+                        {
+                            ID = Convert.ToInt32(row.Cells[0].Value),
+                            DataIncio = row.Cells[1].Value.ToString(),
+                            DataFim = row.Cells[2].Value.ToString(),
+                            Observacoes = row.Cells[3].Value.ToString()
+                        });
+                    }
+
+                    DateTime minDataInicio = listaItens.Min(x => ConvertInfoData(x.DataIncio)).Date,
+                             maxDataFim = listaItens.Max(x => ConvertInfoData(x.DataFim)).Date,
+                             datainicio = ConvertInfoData(dtpInicio.Text).Date,
+                             dataFim = ConvertInfoData(dtpFim.Text).Date;
+
+                    bool flagPermiteAfastamento = false;
+
+                    if (dataFim < minDataInicio)
+                        flagPermiteAfastamento = true;
+
+                    if (datainicio > maxDataFim)
+                        flagPermiteAfastamento = true;
+
+                    if (flagPermiteAfastamento)
+                        dgvDados.Rows.Add(dgvDados.Rows.Count + 1, dtpInicio.Value.ToString("dd/MM/yyyy"), dtpFim.Value.ToString("dd/MM/yyyy"), txtObservations.Text);
+                    else
+                        MessageBox.Show("Já existe um agendamento ativo ou agendado para as datas informadas", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                    //Final
+                    //ResetDatas();
+                }                
+            }
+            else
+            {
+                MessageBox.Show("Favor preencher as observações", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private DateTime ConvertInfoData(string textoData)
+        {
+            return Convert.ToDateTime(textoData.Substring(3, 2) + "-" + textoData.Substring(0, 2) + "-" + textoData.Substring(6, 4));
+        }
+
+        private void ResetDatas()
+        {
+            this.dtpInicio.Value = DateTime.Now.AddDays(5);
+            this.dtpFim.Value = dtpInicio.Value.AddDays(7);
+        }
+    }
+}
